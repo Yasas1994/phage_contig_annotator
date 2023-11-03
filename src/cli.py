53e3aa6d652237ae10af0f0cfa72e16727ed999c
Yasas1994 <yasas.wijesekara@gmail.com>
2023-11-03 13:35:51 +0100

    added new db link

--- src/cli.py
+++ src/cli.py
@@ -11,33 +11,38 @@ import configparser
 def download_dbs(path):
     if not Path(path).joinpath('db_chkpt').exists():
         import requests
-        import zipfile
-        url =  "https://nextcloud.uni-greifswald.de/index.php/s/w2pgjQXdifsCtGA/download/databases.zip"
+        import tarfile
+        url =  "https://nextcloud.uni-greifswald.de/index.php/s/ft8FAoQXscoj9eo/download/database.tar.gz"
         # Ensure the download directory exists
         os.makedirs(path, exist_ok=True)
 
         # Download the zip file
         response = requests.get(url)
         if response.status_code == 200:
-            zip_file_path = os.path.join(path, 'database.zip')
-            with open(zip_file_path, 'wb') as file:
-                file.write(response.content)
+            tar_file_path = os.path.join(path, 'database.tar.gz')
+
+            with open(tar_file_path, "wb") as file:
+                for chunk in response.iter_content(chunk_size=8192):
+                    if chunk:
+                        file.write(chunk)
+
 
             # Ensure the extraction directory exists
             os.makedirs(path, exist_ok=True)
 
-            # Extract the zip file
-            with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
-                zip_ref.extractall(path)
 
-            # Clean up the downloaded zip file
-            os.remove(zip_file_path)
+            # Extract the tar.gz file
+            with tarfile.open(tar_file_path, "r:gz") as tar:
+                tar.extractall(path=path)
 
-            logger.info(f"File downloaded and extracted to {path}")
+                # Clean up the downloaded zip file
+            os.remove(tar_file_path)
+
+            logger.info(f"database downloaded and extracted to {path}")
             Path(path).joinpath('db_chkpt').touch()
             return True
         else:
-            logger.info(f"Failed to download the file from {url}. Status code: {response.status_code}")
+            logger.info(f"Failed to download the database from {url}. Status code: {response.status_code}")
             return False
     else:
         logger.info(f"Skipping database download as checkpoint found at {path}")
@@ -171,17 +176,17 @@ def main():
         if 'hmmerdb' in path:
             logger.info('hmmerdb found')
             db_name = path.split('/')[-1].split('_')[0]
-            hmmerdb[db_name] = f"{path}"
+            hmmerdb[db_name] = path
 
         if 'blastdb' in path:
             logger.info('blastdb found')
             db_name = dbname(path)
-            blastdb = f"{path}/{db_name}"
+            blastdb = os.path.join(path,db_name)
 
         if 'diamond' in path:
             logger.info('diamond_db found')
             db_name = dbname(path)
-            diamonddb = f"{path}/{db_name}"
+            diamonddb = os.path.join(path,db_name)
 
         if 'meta' in path:
             logger.info('metadata file found')
@@ -189,17 +194,17 @@ def main():
 
     #tmp dirs and file names
     fname = args.input.split('/')[-1].rsplit('.',1)[0]
-    tmp_dir = f'{os.path.abspath(args.output)}/{fname}'
+    tmp_dir = os.path.join(os.path.abspath(args.output),fname)
 
-    fna_dir = f'{tmp_dir}/fna'
-    prot_dir = f"{tmp_dir}/proteins" #this can be done inside the call genes funciton
+    fna_dir = os.path.join(tmp_dir,'fna')
+    prot_dir = os.path.join(tmp_dir, 'proteins') #this can be done inside the call genes funciton
 
     hmmsearch_dirs={}
     for key in hmmerdb.keys():
-        hmmsearch_dirs[key] = f"{tmp_dir}/hmmsearch_{key}"
+        hmmsearch_dirs[key] = os.path.join(tmp_dir,f"hmmsearch_{key}")
 
-    plots_dir= f"{tmp_dir}/plots"
-    combine_dir = f"{tmp_dir}/combined"
+    plots_dir= os.path.join(tmp_dir,'plots')
+    combine_dir = os.path.join(tmp_dir,'combined')
 
     #creating dir
     for i in hmmsearch_dirs.values():
@@ -211,7 +216,7 @@ def main():
     if args.type != 'contigs':
         #create a symlobic link to the input file in tmp dir
         try:
-            os.symlink(os.path.abspath(args.input), f"{tmp_dir}/proteins.faa")
+            os.symlink(os.path.abspath(args.input), os.path.join(tmp_dir,'proteins.faa'))
         except:
             logger.warning('symlink exists')
         
@@ -222,20 +227,41 @@ def main():
     if args.command == 'runall':
         if args.contigs:
             
-            call_genes(f'{args.input}', threads=args.cpus, tmp_dir=tmp_dir, trna=True)
+            call_genes( f'{args.input}',
+                        threads=args.cpus,
+                        tmp_dir=tmp_dir, 
+                        trna=True)
         for key in hmmerdb.keys(): #run hmmsearch for all available databases 
-            search_hmms(hmmout_dir=hmmsearch_dirs[key],proteins_dir=prot_dir, threads=args.cpus, db_dir=hmmerdb[key], tmp_dir=tmp_dir)
-        generate_plots_and_gff(tmp_dir=tmp_dir,trna_dir=f'{tmp_dir}/trna.tsv',  hmmsearch_dir=f'{tmp_dir}/hmmsearch.txt', meta_dir=meta_path, gff_dir=gff_path,) 
+            search_hmms(hmmout_dir=hmmsearch_dirs[key],
+                        proteins_dir=prot_dir,
+                        threads=args.cpus,
+                        db_dir=hmmerdb[key], 
+                        tmp_dir=tmp_dir)
+        generate_plots_and_gff( tmp_dir=tmp_dir,trna_dir=f'{tmp_dir}/trna.tsv',
+                                hmmsearch_dir=f'{tmp_dir}/hmmsearch.txt',
+                                meta_dir=meta_path,
+                                gff_dir=gff_path,) 
 
     elif args.command == 'custom':
         if args.run_prodigal:
-            call_genes(f'{args.input}', threads=args.cpus,tmp_dir=tmp_dir,trna=args.run_trnascan)
+            call_genes(f'{args.input}',
+                        threads=args.cpus,
+                        tmp_dir=tmp_dir,
+                        trna=args.run_trnascan)
         if args.run_hmmer:
             for key in hmmerdb.keys():
-                search_hmms(hmmout_dir=hmmsearch_dirs[key],proteins_dir=prot_dir, threads=args.cpus, db_dir=hmmerdb[key], tmp_dir=tmp_dir)
+                search_hmms(hmmout_dir=hmmsearch_dirs[key],
+                            proteins_dir=prot_dir,
+                            threads=args.cpus,
+                            db_dir=hmmerdb[key],
+                            tmp_dir=tmp_dir)
 
         if args.run_combine:
-            code = run_combine(protein_gff=f"{tmp_dir}/proteins.gff", trna_tsv=f"{tmp_dir}/trna.tsv", hmmsearch=f"{tmp_dir}/hmmsearch.csv", annotation=meta_path,output=f"{tmp_dir}/{fname}.csv",out=combine_dir)
+            code = run_combine( protein_gff=os.path.join(tmp_dir,'proteins.gff'), 
+                                trna_tsv=os.path.join(tmp_dir,'trna.tsv'),
+                                hmmsearch=os.path.join(tmp_dir,'hmmsearch.csv'),
+                                annotation=meta_path,output=os.path.join(tmp_dir,f"{fname}.csv"),
+                                out=combine_dir )
             if not code:
                 logger.error('summarizing failed')
 
